037cb849589be21fe50a3f8d752d09e64fe4370e
romanolucasandres <romanolucasandres@gmail.com>
2022-12-06 20:02:12 -0300

    Cambios

---
+++ Sueldo/Program.cs
@@ -0,0 +1,18 @@
+﻿float sueldo;
+string linea;
+
+Console.WriteLine("Ingrese el sueldo: ");
+linea = Console.ReadLine();
+
+sueldo = float.Parse(linea);
+
+if(sueldo > 3000)
+{
+    Console.WriteLine("Esta persona debe abonar impuestos");
+}
+else
+{
+    Console.WriteLine("No debe pagar impuestos");
+}
+
+Console.ReadKey();
